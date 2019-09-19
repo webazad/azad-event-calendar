38f24cef922fedee36001b47047926abced4fcf8
Azad <azad@azad.com>
2019-09-19 11:50:35 +0600

    Last...

--- azad-event-calendar.php
+++ azad-event-calendar.php
@@ -1,18 +1,18 @@
 <?php
 /* 
 Plugin Name: Azad Event Calendar
-Description: A very simple gutenberg practice.
-Plugin URi: gittechs.com/plugin/azad-gutenberg 
+Description: A very simple event calendar practice.
+Plugin URi: gittechs.com/plugin/azad-event-calendar 
 Author: Md. Abul Kalam Azad
 Author URI: gittechs.com/author
 Author Email: webdevazad@gmail.com
 Version: 0.0.0.1
 License: GPL2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: azad-gutenberg
+Text Domain: azad-event-calendar
 Domain Path: /languages
 
-@package: azad-gutenberg
+@package: azad-event-calendar
 */
 if(! defined('ABSPATH')) exit;
 
@@ -28,7 +28,7 @@ require_once dirname( TRIBE_EVENTS_FILE ) . '/vendor/autoload.php';
  * Verifies if we need to warn the user about min PHP version and bail to avoid fatals
  */
 if ( tribe_is_not_min_php_version() ) {
-//	tribe_not_php_version_textdomain( 'the-events-calendar', TRIBE_EVENTS_FILE );
+	tribe_not_php_version_textdomain( 'the-events-calendar', TRIBE_EVENTS_FILE );
 
 	/**
 	 * Include the plugin name into the correct place
@@ -46,7 +46,7 @@ if ( tribe_is_not_min_php_version() ) {
 
 	add_filter( 'tribe_not_php_version_names', 'tribe_events_not_php_version_plugin_name' );
 	if ( ! has_filter( 'admin_notices', 'tribe_not_php_version_notice' ) ) {
-		//add_action( 'admin_notices', 'tribe_not_php_version_notice' );
+		add_action( 'admin_notices', 'tribe_not_php_version_notice' );
 	}
 	return false;
 }
@@ -56,7 +56,7 @@ if ( tribe_is_not_min_php_version() ) {
  */
 require_once dirname( TRIBE_EVENTS_FILE ) . '/src/Tribe/Main.php';
 
-//Tribe__Events__Main::instance();
+Tribe__Events__Main::instance();
 
 //register_activation_hook( TRIBE_EVENTS_FILE, array( 'Tribe__Events__Main', 'activate' ) );
 //register_deactivation_hook( TRIBE_EVENTS_FILE, array( 'Tribe__Events__Main', 'deactivate' ) );
